4c40fcc8eb652d2037eada4d6d0eafbf94b63036
rougee <rjyang118@gmail.com>
2015-04-30 12:03:49 +1000

    Revert "Revert "Allahu akbar""
    This reverts commit 8c8f3b0492c07951412a9035101991b7d849435b.
    
    Revert "Allahu akbar"
    
    This reverts commit
    5038edb0a90a75b33684a3e5ff21fc84c178ac0f.
    
    Allahu akbar

--- moveEntity.h
+++ moveEntity.h
@@ -10,7 +10,7 @@
 #define LEFT 2
 
 //revision required later so that attempted movement into a monster entity results in attack function being called
-int move(char* entityArray[][15], char* mapArray[][15], int direction, int entityx, int entityy, int entityIndex) { 
+int move(char* entityArray[][15], char* mapArray[][15], int direction, int entityx, int entityy, int entityIndex) {
 //entityx is the x value in the array of the entity, similarly for entityy
    int valid;
    valid = obstructionCheck(entityArray[][15], mapArray[][15], direction, entityx, entityy);
@@ -20,7 +20,7 @@ int move(char* entityArray[][15], char* mapArray[][15], int direction, int entit
          entityInfo[entityIndex,ENTITY_Y]++;
          entityArray[entityx][entityy+1] = entityArray[entityx][entityy];
          entityArray[entityx][entityy] = NO_ENTITY;
-      } else if (direction == DOWN) { 
+      } else if (direction == DOWN) {
          entityInfo[entityIndex,ENTITY_Y]--;
          entityArray[entityx][entityy-1] = entityArray[entityx][entityy];
          entityArray[entityx][entityy] = NO_ENTITY;
@@ -30,7 +30,7 @@ int move(char* entityArray[][15], char* mapArray[][15], int direction, int entit
          entityArray[entityx][entityy] = NO_ENTITY;
       } else if (direction == LEFT) {
          entityInfo[entityIndex,ENTITY_X]--;
-         entityArray[entity-1][entityy] = entityArray[entityx][entityy];
+         entityArray[entityx-1][entityy] = entityArray[entityx][entityy];
          entityArray[entityx][entityy] = NO_ENTITY;
       }
       return EXIT_SUCCESS;
@@ -40,5 +40,18 @@ int move(char* entityArray[][15], char* mapArray[][15], int direction, int entit
 }
 
 int obstructionCheck(char* entityArray[][15], char* mapArray[][15], int direction, int entityx, int entityy) {
-   //returns TRUE if move is valid, else returns FALSE if move is obstructed by tile or entity 
+   //returns TRUE if move is valid, else returns FALSE if move is obstructed by tile or entity
+   if (direction == UP && mapArray[entityx][entityy+1] == EMPTY_SPACE) {
+      return TRUE;
+   } else if (direction == DOWN && mapArray[entityx][entityy-1] == EMPTY_SPACE){
+      return TRUE;
+   } else if (direction == RIGHT && mapArray[entityx+1][entityy] == EMPTY_SPACE){
+      return TRUE;
+   } else if (direction == LEFT && mapArray[entityx-1][entity] == EMPTY_SPACE){
+      return TRUE;
+   } else {
+       return FALSE;
+   }
+   return EXIT_SUCCESS;
 }
+
