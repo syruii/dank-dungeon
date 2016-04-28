d3be93cbec09091dd72c11c9a1e87db713e28855
syruii <countrproductive@gmail.com>
2016-04-28 15:35:50 +1000

    Create entityPopulate.h

---
+++ entityPopulate.h
@@ -0,0 +1,13 @@
+#ifndef ENITTY.H
+#define ENTITY.H
+
+#define NO_ENTITY '0'
+#define BASE_HP 8
+
+void generatePosition (int roomWidth, int roomHeight, int* posx, int* posy);
+void placeMonster (char entityArray[MAP_SIZE][MAP_SIZE], entity entityInfo[], char mapArray[MAP_SIZE][MAP_SIZE], int roomWidth, int roomHeight, int monNumber);
+void generateMonster (char entityArray[MAP_SIZE][MAP_SIZE], entity entityInfo[], int playerLevel, int monNumber);
+void placePlayer (char entityArray[MAP_SIZE][MAP_SIZE], entity entityInfo[], char mapArray[MAP_SIZE][MAP_SIZE], int roomWidth, int roomHeight);
+
+
+#endif
