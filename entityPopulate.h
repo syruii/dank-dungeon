6a7f568bfe3be0f89ae4dd6594ec873f730b3a99
syruii <countrproductive@gmail.com>
2016-04-28 15:53:11 +1000

    Update entityPopulate.h

--- entityPopulate.h
+++ entityPopulate.h
@@ -3,6 +3,7 @@
 
 #define NO_ENTITY '0'
 #define BASE_HP 8
+#include "main.h"
 
 void generatePosition (int roomWidth, int roomHeight, int* posx, int* posy);
 void placeMonster (char entityArray[MAP_SIZE][MAP_SIZE], entity entityInfo[], char mapArray[MAP_SIZE][MAP_SIZE], int roomWidth, int roomHeight, int monNumber);
