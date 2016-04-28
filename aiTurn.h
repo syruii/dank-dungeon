13327160ddb029941845d688311b49637dadff06
syruii <countrproductive@gmail.com>
2016-04-28 15:51:50 +1000

    Update aiTurn.h

--- aiTurn.h
+++ aiTurn.h
@@ -1,6 +1,8 @@
 #ifndef AITURN_H
 #define AITURN_H
 
+#include "main.h"
+
 int abs(int a);
 int aiPath(entity entityInfo[MAX_ENTITIES], char entityArray[MAP_SIZE][MAP_SIZE], int monsterIndex); //always returns a valid direction
 int playerDistanceSquared (entity entityInfo[MAX_ENTITIES], int monsterIndex);
