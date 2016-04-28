290e5e7d8591812c37cbcc163e8197bad623178a
syruii <countrproductive@gmail.com>
2016-04-28 15:40:04 +1000

    Update moveEntity.c

--- moveEntity.c
+++ moveEntity.c
@@ -4,14 +4,9 @@
    Move function requires entity symbol to be already loaded into array
 */
 
-#define UP 0
-#define DOWN 1
-#define RIGHT 2
-#define LEFT 3
+#include "moveEntity.h"
+#include "main.h"
 
-//revision required later so that attempted movement into a monster entity results in attack function being called
-int move(int roomWidth, int roomHeight, char entityArray[MAP_SIZE][MAP_SIZE], char mapArray[MAP_SIZE][MAP_SIZE], int direction, int entityIndex, entity entityInfo[MAX_ENTITIES]) {
-//entityx is the x value in the array of the entity, similarly for entityy
    /*int valid;
        valid = entityCheck(x, y, entityArray, entityInfo);
 
