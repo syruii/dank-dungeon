1fdc4c01d4050acf72df9d35dacdd287689d41f8
syruii <countrproductive@gmail.com>
2016-04-28 15:43:34 +1000

    Update death.c

--- death.c
+++ death.c
@@ -1,6 +1,9 @@
 //cleans up dead bodies
 //takes in a entity index
 //called from a death check function which will also hand out the exp
+#include "death.h"
+#include "main.h"
+
 void death(int deadIndex,char entityArray[MAP_SIZE][MAP_SIZE], entity entityInfo[MAX_ENTITIES]) {
    entityArray[entityInfo[deadIndex].entityx][entityInfo[deadIndex].entityy] = NO_ENTITY;
    entityInfo[deadIndex].dead = TRUE;
