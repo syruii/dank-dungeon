8d77305b409f0f87ad24cb87237d3579e2acf9d4
syruii <countrproductive@gmail.com>
2016-04-28 15:57:51 +1000

    Update moveEntity.h

--- moveEntity.h
+++ moveEntity.h
@@ -1,7 +1,4 @@
-#define UP 0
-#define DOWN 1
-#define RIGHT 2
-#define LEFT 3
+#include "main.h"
 
 //revision required later so that attempted movement into a monster entity results in attack function being called
 int move(int roomWidth, int roomHeight, char entityArray[MAP_SIZE][MAP_SIZE], char mapArray[MAP_SIZE][MAP_SIZE], int direction, int entityIndex, entity entityInfo[MAX_ENTITIES]) {
