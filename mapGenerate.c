42efdf8382145ca5fe1d18852a0a8373567bc06a
syruii <countrproductive@gmail.com>
2016-04-28 15:38:09 +1000

    Update mapGenerate.c

--- mapGenerate.c
+++ mapGenerate.c
@@ -3,16 +3,9 @@
    revised on 04/05 with addition of stair placement
 */
 
-#include <time.h>
-
-#define VERTICAL_WALL "|"
-#define HORIZONTAL_WALL "-"
-#define MAP_SIZE 15
-
-void printMap(char mapArray[MAP_SIZE][MAP_SIZE], char entityArray[MAP_SIZE][MAP_SIZE], int roomWidth, int roomHeight);  //have to pass second dimension of array
-//void generateMap(int* Width, int* Height);
-void printHorizontalBound(int roomWidth);
-//void placeStairs(char mapArray, int roomWidth, int roomHeight);
+#include "mapGenerate.h"
+#include <stdio.h>
+#include "main.h"
 
 /*
 int main () {
