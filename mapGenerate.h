c405bf6d4474eeb5d2b379c004524cca0b97d96d
syruii <countrproductive@gmail.com>
2016-04-28 15:53:45 +1000

    Update mapGenerate.h

--- mapGenerate.h
+++ mapGenerate.h
@@ -3,9 +3,7 @@
 
 #include <time.h>
 
-#define VERTICAL_WALL "|"
-#define HORIZONTAL_WALL "-"
-#define MAP_SIZE 15
+#include "main.h"
 
 void printMap(char mapArray[MAP_SIZE][MAP_SIZE], char entityArray[MAP_SIZE][MAP_SIZE], int roomWidth, int roomHeight);  //have to pass second dimension of array
 //void generateMap(int* Width, int* Height);
