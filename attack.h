6f34fe7f4e1effc1754828711bcc871451fcdd51
syruii <countrproductive@gmail.com>
2016-04-28 15:32:21 +1000

    Update attack.h

--- attack.h
+++ attack.h
@@ -1,8 +1,6 @@
 #ifndef ATTACK_H
 #define ATTACK_H
 
-#include "main.h"
-
 #define SUCCESS 1
 
 void damageCalc(entity entityInfo[MAX_ENTITIES],char entityArray[MAP_SIZE][MAP_SIZE], int attackerIndex, int defenderIndex, game* gameInfo);
